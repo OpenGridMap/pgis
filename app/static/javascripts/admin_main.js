70ccf84062eb8bb95e51cf9f8f75256361095015
Shota Bakuradze <shotabakuradze@gmail.com>
2015-03-08 18:56:24 +0100

    Styling fix, add dynamic edit for powerlines

--- app/static/javascripts/admin_main.js
+++ app/static/javascripts/admin_main.js
@@ -1,29 +1,15 @@
-$(document).ready(function(){
-	L.Icon.Default.imagePath = APP_IMAGES_URL;
-
-
-	var map = L.map('map').setView([48.1333, 11.5667], 13);
-
-	var powerline = $(map.getContainer()).data('powerline');
-
-	if(powerline != ""){
-		var points_coords = powerline.split(",");
-		var coords = [];
-		for(var i = 0; i < points_coords.length; i++){
-			var parts = points_coords[i].split(" ");
-			coords[i] = []
-			for(var j = 0; j < parts.length; j++){
-				if(parts[j] != ""){
-					coords[i].push(parseFloat(parts[j]));
-				}
-			}			
-		}
-		console.log(coords)
-		var polyline = L.polyline(coords, {color: 'red'}).addTo(map);
+function latlngStringToCoords(powerline){
+	var points_coords = powerline.split(",");
+	var coords = [];
+	for(var i = 0; i < points_coords.length; i++){
+		var parts = points_coords[i].split(" ");
+		coords[i] = []
+		for(var j = 0; j < parts.length; j++){
+			if(parts[j] != ""){
+				coords[i].push(parseFloat(parts[j]));
+			}
+		}			
 	}
 
-	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		maxZoom: 18
-	}).addTo(map);
-})
+	return coords;
+}
