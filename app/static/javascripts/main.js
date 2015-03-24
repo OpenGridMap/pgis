ede32c5165b08e0222bf22cd44814d95637943d1
Shota Bakuradze <shotabakuradze@gmail.com>
2015-03-25 00:17:55 +0100

    Add clustering for points

--- app/static/javascripts/main.js
+++ app/static/javascripts/main.js
@@ -5,31 +5,26 @@ $(document).ready(function(){
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		maxZoom: 18
 	}).addTo(map);
-
-	map.on('moveend', function(e){
-		if(map.getZoom() > 7) {
-			loadMapFragment()
-		}
-	})
+	
+	var markers = new L.MarkerClusterGroup();
+	loadMapFragment();
+	map.addLayer(markers);
 
 	$.ajax({
 		url : "/powerlines",
 		success : function(data){
 			for(var i = 0; i < data.length; i++){
-				console.log(data[i])
 				var polyline = L.polyline(data[i], {color: 'red'}).addTo(map);
 			}
 		}
 	});
 
-	loadMapFragment();
-
 	function loadMapFragment(){
 		$.ajax({
-			url : "/points?bounds=" + latLngBoundsToStr(map.getBounds()),
+			url : "/points",
 			success : function(data){
 				for(var i = 0; i < data.length; i++){
-					var marker = L.marker(data[i]).addTo(map);
+					markers.addLayer(new L.Marker(data[i]));
 				}			
 			}
 		});
