81822d87ed559f87066fc87d94f21191036bd012
Shota Bakuradze <shotabakuradze@gmail.com>
2015-03-28 11:32:37 +0100

    Add leaflet draw, add initial setup for new and edit of point

--- app/__init__.py
+++ app/__init__.py
@@ -14,14 +14,14 @@ assets = Environment(GisApp)
 js = Bundle('javascripts/leaflet-src.js', 'javascripts/jquery-2.1.3.min.js', 'javascripts/main.js', 'javascripts/leaflet.markercluster-src.js',
             filters='jsmin', output='gen/packed.js')
 
-admin_js = Bundle('javascripts/leaflet-src.js', 'javascripts/jquery-2.1.3.min.js', 'javascripts/admin_main.js',
+admin_js = Bundle('javascripts/leaflet-src.js', 'javascripts/jquery-2.1.3.min.js', 'javascripts/admin_main.js', 'javascripts/leaflet.draw-src.js',
             filters='jsmin', output='gen/admin_packed.js')
 assets.register('application_js', js)
 assets.register('admin_application_js', admin_js)
 
 less = Bundle('stylesheets/leaflet.css', 'stylesheets/main.less.css', 'stylesheets/MarkerCluster.css', 'stylesheets/MarkerCluster.Default.css',
               filters='less,cssmin', output='gen/packed.css')
-admin_less = Bundle('stylesheets/leaflet.css', 'stylesheets/admin_main.less.css',
+admin_less = Bundle('stylesheets/leaflet.css', 'stylesheets/admin_main.less.css','stylesheets/leaflet.draw.css',
               filters='less,cssmin', output='gen/admin_packed.css')
 
 assets.register('application_css', less)
