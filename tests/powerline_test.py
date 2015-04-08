67f9dd05df3f1de2d40095d004a307352629fd9c
Shota Bakuradze <shotabakuradze@gmail.com>
2015-04-08 13:05:13 +0200

    Use flask-testing

---
+++ tests/powerline_test.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app import GisApp
+import unittest
+from flask.ext.testing import TestCase
+
+class MyTest(TestCase):
+
+    def create_app(self):
+        GisApp.config['TESTING'] = True
+        # Default port is 5000
+        GisApp.config['LIVESERVER_PORT'] = 8943
+        return GisApp 
+
+    def test_test(self):
+        response = self.client.get("/powerlines")
+        self.assertEquals(response.json, dict(success=True))
+
+ 
