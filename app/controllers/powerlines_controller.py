5e931a2403a13fbdb9c8e027db65863582f72690
Shota Bakuradze <shotabakuradze@gmail.com>
2015-06-27 14:37:44 +0200

    Use intersects to better display bounded powerlines

--- app/controllers/powerlines_controller.py
+++ app/controllers/powerlines_controller.py
@@ -13,9 +13,9 @@ class PowerlinesController:
         powerlines = []
 
         if int(zoom) < 9:
-            powerlines = Powerline.query.filter(func.ST_Contains(func.ST_MakeEnvelope(bounds_parts[1], bounds_parts[0], bounds_parts[3], bounds_parts[2]), Powerline.geom)).filter(func.ST_Length(func.ST_GeographyFromText(func.ST_AsText(Powerline.geom))) > 8000 * (10 - zoom)).all()
+            powerlines = Powerline.query.filter(func.ST_Intersects(func.ST_MakeEnvelope(bounds_parts[1], bounds_parts[0], bounds_parts[3], bounds_parts[2]), Powerline.geom)).filter(func.ST_Length(func.ST_GeographyFromText(func.ST_AsText(Powerline.geom))) > 8000 * (10 - zoom)).all()
         else:
-            powerlines = Powerline.query.filter(func.ST_Contains(func.ST_MakeEnvelope(bounds_parts[1], bounds_parts[0], bounds_parts[3], bounds_parts[2]), Powerline.geom)).all()
+            powerlines = Powerline.query.filter(func.ST_Intersects(func.ST_MakeEnvelope(bounds_parts[1], bounds_parts[0], bounds_parts[3], bounds_parts[2]), Powerline.geom)).all()
 
         powerlines = list(map(lambda powerline: powerline.serialize(), powerlines))
         return Response(json.dumps(powerlines),  mimetype='application/json')
