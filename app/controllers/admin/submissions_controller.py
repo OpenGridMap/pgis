ddb30d811a9ba617c10d61f1c4c3ca3ff89e28da
Shota Bakuradze <shotabakuradze@gmail.com>
2015-07-19 09:46:25 +0200

    Add grouping for submissions

--- app/controllers/admin/submissions_controller.py
+++ app/controllers/admin/submissions_controller.py
@@ -2,12 +2,13 @@ from flask import render_template, flash, redirect, abort, session, url_for, req
 from app import db
 import app.helpers.point_form
 from app.models.point import Point
+from geoalchemy2 import Geometry, func
 
 class SubmissionsController:
     def index(self):
         page = int(request.args.get('page') or 1) 
-        points = Point.query.filter(Point.revised == False).paginate(page, 20)
-        return render_template('admin/points/index.html', points=points)
+        submissions = Point.query.with_entities(Point.submission_id, func.count(Point.id).label("count_points")).filter(Point.revised == False).group_by(Point.submission_id).paginate(page, 20)
+        return render_template('admin/submissions/index.html', submissions=submissions)
 
     def delete(self, id):
         point = Point.query.get(id)
