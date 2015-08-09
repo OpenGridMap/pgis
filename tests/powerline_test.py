9ec5c0622bfdffb82e91e9dd24f97bf053547c60
Shota Bakuradze <shotabakuradze@gmail.com>
2015-08-10 00:15:50 +0200

    Add test for shape method

--- tests/powerline_test.py
+++ tests/powerline_test.py
@@ -7,6 +7,7 @@ from flask_environments import Environments
 from tests.factories.powerline import PowerlineFactory
 from app import db
 from app.models.powerline import Powerline
+import shapely
 
 class MyTest(TestCase):
 
@@ -17,3 +18,11 @@ class MyTest(TestCase):
         PowerlineFactory();
         powerline = db.session.query(Powerline).first()
         self.assertEquals(powerline.serialize(), { 'tags' : [], 'id' : powerline.id, 'latlngs' : [(48.11, 10.1), (59.12, 10.15)] })
+
+    def test_shape(self):
+        PowerlineFactory()
+        powerline = db.session.query(Powerline).first()
+        assert(type(powerline.shape()) is shapely.geometry.linestring.LineString)
+        self.assertEquals(list(powerline.shape().coords), [(48.11, 10.1), (59.12, 10.15)])
+
+        
