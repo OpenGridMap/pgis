70bcdb0ff78aa86e976ceabefd4b3c506df1024a
Shota Bakuradze <shotabakuradze@gmail.com>
2015-08-14 11:05:59 +0200

    Add midpoint calculation

--- app/controllers/admin/submissions_controller.py
+++ app/controllers/admin/submissions_controller.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, abort, session, url_for, request, g, json, Response
 from app import db
-import app.helpers.point_form
+from app.helpers.point_form import PointForm
 from app.models.point import Point
 from app.models.submission import Submission
 from geoalchemy2 import Geometry, func
 from flask.ext.login import current_user
+from sqlalchemy.sql import text
 
 class SubmissionsController:
     def index(self):
@@ -13,8 +14,11 @@ class SubmissionsController:
         return render_template('admin/submissions/index.html', submissions=submissions)
 
     def revise(self, id):
+        form = PointForm() 
         submission = Submission.query.get(id)
-        return render_template('admin/submissions/revise.html', submission=submission)
+        query = text("SELECT ST_X(ST_CENTROID(ST_COLLECT(geom))), ST_Y(ST_CENTROID(ST_COLLECT(geom))) FROM point WHERE submission_id=:submission_id")
+        mid_point = list(db.engine.execute(query, submission_id=32).first())
+        return render_template('admin/submissions/revise.html', submission=submission, form=form, mid_point=mid_point)
 
     def merge(self, id):
         pass
