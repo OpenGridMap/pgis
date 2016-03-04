76bdf07329fd44df8bb54a919b156175a6a5b840
klausschreiber <mail@schreiberklaus.de>
2016-03-04 02:18:17 +0100

    Drop jQuery1, set jQuery2 as default because there is no reason to load both versions and it makes problems with bootstrap modal javascript So support for IE6-IE8 is dropped

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,20 @@ from flask.ext.assets import Environment, Bundle
 from flask_environments import Environments
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+
+# for using a current jQuery version from cdn instead of jQuery 1
+from flask_bootstrap import WebCDN
+
 import os
 
 # Create app
 GisApp = Flask(__name__)
 Bootstrap(GisApp)
+# for using jQuery 2 from cdn instead of jQuery 1
+# (https://github.com/mbr/flask-bootstrap/blob/master/docs/faq.rst#how-can-i-use-jquery2-instead-of-jquery1)
+GisApp.extensions['bootstrap']['cdns']['jquery'] = WebCDN(
+    '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/'
+)
 # GisApp.config.from_object('config')
 db = SQLAlchemy(GisApp)
 
@@ -19,7 +28,7 @@ assets = Environment(GisApp)
 js = Bundle('javascripts/leaflet-src.js', 'javascripts/leaflet.markercluster-src.js','javascripts/handlebars-v3.0.3.js', 'javascripts/Control.Geocoder.js','javascripts/Control.LinkButton.js','javascripts/underscore-min.js','javascripts/L.Control.Sidebar.js', 'javascripts/Control.Loading.js', 'javascripts/main.js',
             filters='jsmin', output='gen/packed.js')
 
-admin_js = Bundle('javascripts/leaflet-src.js', 'javascripts/jquery-2.1.3.min.js', 'javascripts/admin_main.js', 'javascripts/leaflet.draw-src.js','javascripts/handlebars-v3.0.3.js','javascripts/underscore-min.js', 'javascripts/leaflet.markercluster-src.js',
+admin_js = Bundle('javascripts/leaflet-src.js', 'javascripts/admin_main.js', 'javascripts/leaflet.draw-src.js','javascripts/handlebars-v3.0.3.js','javascripts/underscore-min.js', 'javascripts/leaflet.markercluster-src.js',
             filters='jsmin', output='gen/admin_packed.js')
 assets.register('application_js', js)
 assets.register('admin_application_js', admin_js)
