03e9452e8a2fd68d8e18097ed576a9533727814f
Sri Vishnu Totakura <Munna@Sri-Vishnu-Totakuras-MacBook-Pro.local>
2016-05-12 11:22:51 +0200

    indendation

--- app/static/javascripts/main.js
+++ app/static/javascripts/main.js
@@ -1,12 +1,12 @@
 var getQueryString = function ( field, url ) {
-    var href = url ? url : window.location.href;
-    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
-    var string = reg.exec(href);
-    return string ? string[1] : null;
+  var href = url ? url : window.location.href;
+  var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+  var string = reg.exec(href);
+  return string ? string[1] : null;
 };
 
 $(document).ready(function(){
-	L.Icon.Default.imagePath = APP_IMAGES_URL;
+  L.Icon.Default.imagePath = APP_IMAGES_URL;
 
 
   var lat = getQueryString("lat");
@@ -20,23 +20,25 @@ $(document).ready(function(){
   var zoom = getQueryString("zoom") || 13;
 
   var osm_map = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		maxZoom: 18, minZoom: 8
-	});
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 8
+  });
 
   var satellite_map = L.tileLayer('http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		maxZoom: 18, minZoom: 8
-	});
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 8
+  });
   var map = L.map('map', {
-    layers: [osm_map] 
+    layers: [osm_map]
   }).setView(center, zoom);
   if (!lat && !lng) {
     map.locate({setView : true});
   }
 
   var loadingControl = L.Control.loading({
-      separate: true
+    separate: true
   });
   map.addControl(loadingControl);
 
@@ -46,8 +48,8 @@ $(document).ready(function(){
     bounds = bounds.split("%2C");
     console.log(bounds);
     map.fitBounds([
-        [bounds[0], bounds[1]],
-        [bounds[2], bounds[3]]
+      [bounds[0], bounds[1]],
+      [bounds[2], bounds[3]]
     ]);
   }
 
@@ -55,7 +57,7 @@ $(document).ready(function(){
     "Satellite View": satellite_map,
     "Topological View": osm_map
   };
-	L.Control.geocoder().addTo(map);
+  L.Control.geocoder().addTo(map);
   L.control.layers(baseMaps).addTo(map);
 
   var newPointLinkProperties = {
@@ -66,153 +68,154 @@ $(document).ready(function(){
   var newPointLink = L.control.link_button(newPointLinkProperties).addTo(map);
 
 
-	var markers = new L.MarkerClusterGroup();
-	map.addLayer(markers);
-	var clusterGroup = new L.LayerGroup();
-	map.addLayer(clusterGroup);
-	var powerlines = new L.LayerGroup();
-	map.addLayer(powerlines);
-    // adding sidebar
-    var sidebar = L.control.sidebar('sidebar', {
-        position: 'left'
+  var markers = new L.MarkerClusterGroup();
+  map.addLayer(markers);
+
+  var clusterGroup = new L.LayerGroup();
+  map.addLayer(clusterGroup);
+
+  var powerlines = new L.LayerGroup();
+  map.addLayer(powerlines);
+
+  // adding sidebar
+  var sidebar = L.control.sidebar('sidebar', {
+    position: 'right'
+  });
+  map.addControl(sidebar);
+
+  loadMapFragment();
+  var loadFragmentDebounced = _.debounce(loadMapFragment, 1000);
+  map.on('moveend', function(){
+    loadFragmentDebounced();
+  });
+
+  var markerMap = {};
+
+  //Adapted from https://github.com/Leaflet/Leaflet.markercluster/issues/217#issuecomment-20963103
+  var iconCreateFunction = function (cluster) {
+
+    var count = cluster.count;
+
+    // cluster icon
+    var c = 'marker-cluster-';
+    if (count < 10) {
+      c += 'small';
+    } else if (count < 100) {
+      c += 'medium';
+    } else {
+      c += 'large';
+    }
+
+    return new L.DivIcon({
+      html: '<div><span>' + count + '</span></div>',
+      className: 'marker-cluster ' + c,
+      iconSize: new L.Point(40, 40)
     });
-    map.addControl(sidebar);
+  };
 
-	loadMapFragment();
-
-    var loadFragmentDebounced = _.debounce(loadMapFragment, 1000);
-    map.on('moveend', function(){
-      loadFragmentDebounced();
-    });
-
-    var markerMap = {};
-    
-    //Adapted from https://github.com/Leaflet/Leaflet.markercluster/issues/217#issuecomment-20963103
-    var iconCreateFunction = function (cluster) {
-
-        var count = cluster.count;
-
-        // cluster icon
-        var c = 'marker-cluster-';
-        if (count < 10) {
-            c += 'small';
-        } else if (count < 100) {
-            c += 'medium';
-        } else {
-            c += 'large';
-        }
-
-        return new L.DivIcon({
-            html: '<div><span>' + count + '</span></div>',
-            className: 'marker-cluster ' + c,
-            iconSize: new L.Point(40, 40)
-        });
-    };
-
-	function loadMapFragment(){
-        if(map.getZoom() > 11) {
-            map.fireEvent("dataloading");
-            $.ajax({
-                url : "/points",
-                data: {
-                    "bounds" : map.getBounds().toBBoxString() 
-                },
-                success : function(data){
-                    markers.clearLayers();
-                    clusterGroup.clearLayers();
-                    var newMarkers = []
-                    markerMap = {};
-                    for(var i = 0; i < data.length; i++){
-                        var marker = new L.Marker(data[i]['latlng']).on('click', onMarkerClick);
-                        marker.data = data[i];
-                        newMarkers.push(marker);
-                        markerMap[data[i].id] = marker;
-                    }			
-                    markers.addLayers(newMarkers);
-                    function onMarkerClick(e) {
-                       sidebar.setContent(getPointSidebarContent(e.target.data));
-                       if (!sidebar.isVisible()) {
-                           sidebar.show()
-                       }
-                    }
-                    map.fireEvent("dataload");
-                }
-            });
-        } else {
-            map.fireEvent("dataloading");
-            $.ajax({
-                url : "/points/clustered",
-                data : {
-                    "zoom" : map.getZoom(),
-                    "bounds" : map.getBounds().toBBoxString() 
-                },
-                success : function(data){
-                    markers.clearLayers();
-                    clusterGroup.clearLayers();
-                    for(var i = 0; i < data.length; i++){
-                        var marker = new L.Marker(data[i]['latlng'], {
-                            icon: iconCreateFunction(data[i])
-                        });
-                        marker.panelOpen = false;
-                        clusterGroup.addLayer(marker);
-                        marker.on('click', function(e){
-                            map.setView(e.target.getLatLng(), map.getZoom() + 1);
-                        });
-                    }
-                    map.fireEvent("dataload");
-                }
-            });
-        }
-
-        map.fireEvent("dataloading");
-        $.ajax({
-            url : "/powerlines",
-            data : {
-                "bounds"    : map.getBounds().toBBoxString(),
-                "zoom"      : map.getZoom() 
-            },
-            success : function(data){
-                powerlines.clearLayers();
-                for(var i = 0; i < data.length; i++){
-                    var polyline = L.polyline(data[i].latlngs, {color: 'red'});
-                    bindPowerlinePopup(polyline, data[i]);
-                    powerlines.addLayer(polyline);
-                }
-                map.fireEvent("dataload");
+  function loadMapFragment(){
+    if(map.getZoom() > 11) {
+      map.fireEvent("dataloading");
+      $.ajax({
+        url : "/points",
+        data: {
+          "bounds" : map.getBounds().toBBoxString()
+        },
+        success : function(data){
+          markers.clearLayers();
+          clusterGroup.clearLayers();
+          var newMarkers = []
+          markerMap = {};
+          for(var i = 0; i < data.length; i++){
+            var marker = new L.Marker(data[i]['latlng']).on('click', onMarkerClick);
+            marker.data = data[i];
+            newMarkers.push(marker);
+            markerMap[data[i].id] = marker;
+          }
+          markers.addLayers(newMarkers);
+          function onMarkerClick(e) {
+            sidebar.setContent(getPointSidebarContent(e.target.data));
+            if (!sidebar.isVisible()) {
+              sidebar.show()
             }
-        });
+          }
+          map.fireEvent("dataload");
+        }
+      });
+    } else {
+      map.fireEvent("dataloading");
+      $.ajax({
+        url : "/points/clustered",
+        data : {
+          "zoom" : map.getZoom(),
+          "bounds" : map.getBounds().toBBoxString()
+        },
+        success : function(data){
+          markers.clearLayers();
+          clusterGroup.clearLayers();
+          for(var i = 0; i < data.length; i++){
+            var marker = new L.Marker(data[i]['latlng'], {
+              icon: iconCreateFunction(data[i])
+            });
+            marker.panelOpen = false;
+            clusterGroup.addLayer(marker);
+            marker.on('click', function(e){
+              map.setView(e.target.getLatLng(), map.getZoom() + 1);
+            });
+          }
+          map.fireEvent("dataload");
+        }
+      });
     }
 
-    Handlebars.registerHelper('json', function(context) {
-        return JSON.stringify(context, null, 4);
+    map.fireEvent("dataloading");
+    $.ajax({
+      url : "/powerlines",
+      data : {
+        "bounds"    : map.getBounds().toBBoxString(),
+        "zoom"      : map.getZoom()
+      },
+      success : function(data){
+        powerlines.clearLayers();
+        for(var i = 0; i < data.length; i++){
+          var polyline = L.polyline(data[i].latlngs, {color: 'red'});
+          bindPowerlinePopup(polyline, data[i]);
+          powerlines.addLayer(polyline);
+        }
+        map.fireEvent("dataload");
+      }
     });
+  }
 
-    var source   = $("#marker-sidebar-template").html();
-    var markerSidebarTemplate = Handlebars.compile(source);
-        
-    var source   = $("#polyline-popup-template").html();
-    var polylinePopupTemplate = Handlebars.compile(source);
+  Handlebars.registerHelper('json', function(context) {
+    return JSON.stringify(context, null, 4);
+  });
 
-    /* function bindMarkerPopup(marker, point){
-       var popup = L.popup()
-                    .setContent(markerPopupTemplate(point));
-       marker.bindPopup(popup);
-    } */
+  var source   = $("#marker-sidebar-template").html();
+  var markerSidebarTemplate = Handlebars.compile(source);
 
-    function getPointSidebarContent(pointdata){
-        return markerSidebarTemplate(pointdata);
-    }
+  var source   = $("#polyline-popup-template").html();
+  var polylinePopupTemplate = Handlebars.compile(source);
 
-    function bindPowerlinePopup(polyline, powerline){
-       var popup = L.popup()
-                    .setContent(polylinePopupTemplate(powerline));
-       polyline.bindPopup(popup);
-    }
-    
+  /* function bindMarkerPopup(marker, point){
+     var popup = L.popup()
+     .setContent(markerPopupTemplate(point));
+     marker.bindPopup(popup);
+     } */
 
-    function clusterClickEvent(){
-        
+  function getPointSidebarContent(pointdata){
+    return markerSidebarTemplate(pointdata);
+  }
 
-    }
+  function bindPowerlinePopup(polyline, powerline){
+    var popup = L.popup()
+                  .setContent(polylinePopupTemplate(powerline));
+    polyline.bindPopup(popup);
+  }
+
+  function clusterClickEvent(){
+
+
+  }
 
 });
