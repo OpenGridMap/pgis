070b6c279f9ceb0849f395ed9b307fc8603dded9
klausschreiber <mail@schreiberklaus.de>
2016-06-28 07:54:30 +0200

    fix ranking table

--- app/controllers/ranking_controller.py
+++ app/controllers/ranking_controller.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from app.models.user import User
+from app import db
 
 class RankingController():
 
     def index(self):
-        users = User.query.order_by(User.activity_points.desc()).limit(10).paginate(1)
+        users = db.session.query(User).order_by(User.activity_points.desc()).limit(10).all()
         return render_template('ranking.html', users=users)
