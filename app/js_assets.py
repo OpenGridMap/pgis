40d162fa3afe08fe21d424d6bef92d5510ecd1c1
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-06-28 23:46:20 +0200

    Extend L.MarkerClusterGroup
    This enables us to perform easy highlighting of the MarkerClusterGroup.
    Main usecase is to show the OSM relations on the map and highlight all
    the elements on a mouseover to visually represent elements of a
    relation.

--- app/js_assets.py
+++ app/js_assets.py
@@ -3,6 +3,7 @@ common_js_files = [
     'javascripts/libraries/handlebars-v3.0.3.js',
     'javascripts/libraries/underscore-min.js',
     'javascripts/libraries/leaflet.markercluster-src.js',
+    'javascripts/libraries/MarkerClusterGroup.Refresh.js',
     'javascripts/helpers/misc-helpers.js',
     'javascripts/helpers/handlebars.js'
 ]
@@ -14,10 +15,11 @@ non_admin_js_files = common_js_files + [
     'javascripts/libraries/L.Control.Sidebar.js',
     'javascripts/libraries/Control.Loading.js',
     'javascripts/libraries/Map.SelectArea.min.js',
+    'javascripts/non-admin/leaflet.Pgis.markerClusterGroup.js',
     'javascripts/non-admin/pgis-map.js',
     'javascripts/non-admin/map-helpers.js',
     'javascripts/non-admin/map-data-loader.js',
-    'javascripts/non-admin/main.js'
+    'javascripts/non-admin/main.js',
 ]
 
 admin_js_files = common_js_files + [
---
+++ app/static/javascripts/non-admin/leaflet.Pgis.MarkerClusterGroup.js
@@ -0,0 +1,95 @@
+// Extend the behavior of L.MarkerClusterGroup from
+// https://github.com/Leaflet/Leaflet.markercluster
+// so that we can change the style of the cluster or the markers.
+
+// Author: Sri Vishnu Totakura <t.srivishnu@gmail.com>
+
+L.PgisMarkerClusterGroup = L.MarkerClusterGroup.extend({
+
+  options: {
+    // Add these css classes to the Cluster marker icon when
+    //  clustered. These classes won't appear when Markers are shown.
+    defaultIconCssClasses: ['default'],
+    highlightIconCssClasses: ['highlighted'],
+    // The below option is modified by other functions. Don't pass this
+    //  option while initializing. It is changed dynamically depending on
+    //  +defaultIconCssClasses+ and +highlightIconCssClasses+.
+    clusterIconCssClasses: undefined
+  },
+
+  addedLayers: [],
+
+  // Override the default icon creation function.
+  // Only change intended was to add className to DivIcon dynamically
+  // based on the +clusterIconCssClasses+ options.
+  _defaultIconCreateFunction: function(cluster) {
+     var childCount = cluster.getChildCount();
+
+     var c = ' marker-cluster-';
+
+     if (childCount < 10) {
+       c += 'small';
+     } else if (childCount < 100) {
+       c += 'medium';
+     } else {
+       c += 'large';
+     }
+
+     if(!this.clusterIconCssClasses){
+       this.clusterIconCssClasses = this.defaultIconCssClasses;
+     }
+     var additionalClasses = this.clusterIconCssClasses.join(" ")
+
+     return new L.DivIcon({
+       html: '<div><span>' + childCount + '</span></div>',
+       className: additionalClasses + ' marker-cluster' + c,
+       iconSize: new L.Point(40, 40)
+     });
+   },
+
+   additionalClusterCssClasss: function(){
+     return this.clusterIconCssClasses.join(" ")
+   },
+
+   addHighlightStyle: function(){
+      this.options.clusterIconCssClasses = this.options.highlightIconCssClasses;
+      this.refreshClusters();
+      var _this = this;
+
+      // This assumes all the child layers in this clusterGroup are
+      // markers - L.Marker
+      this.eachLayer(function(layer){
+        layer.setIcon(_this.getMarkerHighlightIcon());
+      })
+   },
+
+   removeHighlightStyle: function() {
+     this.options.clusterIconCssClasses = this.options.defaultIconCssClasses;
+     this.refreshClusters();
+     var _this = this;
+
+     // This assumes all the child layers in this clusterGroup are
+     // markers - L.Marker
+     this.eachLayer(function(layer) {
+       layer.setIcon(_this.getMarkerDefaultIcon());
+     })
+   },
+
+   getMarkerDefaultIcon: function() {
+     return new L.Icon({
+       iconUrl: '/static/images/marker-icon-red.png'
+     });
+   },
+
+   getMarkerHighlightIcon: function() {
+     return new L.Icon({
+       iconUrl: '/static/images/marker-icon.png'
+     });
+   }
+});
+
+L.pgisMarkerClusterGroup = function(options) {
+  return new L.PgisMarkerClusterGroup(options);
+};
+
+
