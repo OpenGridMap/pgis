3d5d80c0db2bd32e72cd52302fd238f641ca0988
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-06-30 22:03:13 +0200

    Improve readability in controllers

--- app/controllers/points_controller.py
+++ app/controllers/points_controller.py
@@ -14,24 +14,74 @@ class PointsController:
     def index(self):
         if request.args.get('bounds') is None:
             return Response(json.dumps([]), mimetype='application/json')
+
         bounds_parts = request.args.get("bounds").split(',')
-        points = Point.query.filter(func.ST_Contains(func.ST_MakeEnvelope(bounds_parts[1], bounds_parts[0], bounds_parts[3], bounds_parts[2]), Point.geom)).filter(or_(Point.approved, Point.revised == False)).all()
+        points = Point.query.filter(
+            func.ST_Contains(
+                func.ST_MakeEnvelope(
+                    bounds_parts[1],
+                    bounds_parts[0],
+                    bounds_parts[3],
+                    bounds_parts[2]),
+                Point.geom
+            )
+        ).filter(
+            or_(Point.approved, Point.revised == False)
+        ).all()
         points = list(map(lambda point: point.serialize(), points))
+
         return Response(json.dumps(points),  mimetype='application/json')
 
     def clustered(self):
         n_clusters = 5
+
         if request.args.get('bounds') is None:
             return Response(json.dumps([]), mimetype='application/json')
-        bounds_parts = request.args.get("bounds").split(',')
+
         if request.args.get('zoom'):
             n_clusters = int(request.args.get('zoom'))
+
+        bounds_parts = request.args.get("bounds").split(',')
+
         #TODO this should be cached or precomputed
-        query = text("SELECT kmeans, count(*), ST_X(ST_Centroid(ST_Collect(geom))), ST_Y(ST_Centroid(ST_Collect(geom))) AS geom FROM ( SELECT kmeans(ARRAY[ST_X(geom), ST_Y(geom)], :n_clusters) OVER (), geom FROM point WHERE ST_Contains(ST_MakeEnvelope(:bounds_1, :bounds_2, :bounds_3, :bounds_4), point.geom) AND (point.approved = TRUE OR point.revised = FALSE)) AS ksub GROUP BY kmeans ORDER BY kmeans;")
-        result = db.engine.execute(query, n_clusters=n_clusters, bounds_1=bounds_parts[1], bounds_2=bounds_parts[0], bounds_3=bounds_parts[3], bounds_4=bounds_parts[2])
+        query = text("""
+            SELECT kmeans, count(*),
+                    ST_X(ST_Centroid(ST_Collect(geom))),
+                    ST_Y(ST_Centroid(ST_Collect(geom))) AS geom
+            FROM (
+                SELECT kmeans(ARRAY[ST_X(geom), ST_Y(geom)], :n_clusters) OVER (),
+                        geom
+                FROM point
+                WHERE ST_Contains(
+                        ST_MakeEnvelope(
+                            :bounds_1,
+                            :bounds_2,
+                            :bounds_3,
+                            :bounds_4
+                        ),
+                        point.geom
+                    )
+                    AND (point.approved = TRUE OR point.revised = FALSE)
+            ) AS ksub
+            GROUP BY kmeans ORDER BY kmeans;
+        """)
+
+        result = db.engine.execute(
+            query,
+            n_clusters = n_clusters,
+            bounds_1   = bounds_parts[1],
+            bounds_2   = bounds_parts[0],
+            bounds_3   = bounds_parts[3],
+            bounds_4   = bounds_parts[2]
+        )
         clusters = []
+
         for row in result:
-             clusters.append({ 'count' : row[1], 'latlng': [float(row[2]), float(row[3])] })
+             clusters.append({
+                 'count' : row[1],
+                 'latlng': [ float(row[2]), float(row[3]) ]
+             })
+
         return Response(json.dumps(clusters),  mimetype='application/json')
 
     def with_properties(self):
@@ -44,13 +94,13 @@ class PointsController:
 
     def edit(self, id):
         point = app.models.point.Point.query.get(id)
-        form = app.helpers.point_form.PointForm(None, point) 
+        form = app.helpers.point_form.PointForm(None, point)
         form.properties.data = json.dumps(form.properties.data) if form.properties.data else ""
         return render_template('points/edit.html', form=form, point=point)
-    
+
     def update(self, id):
         point = app.models.point.Point.query.get(id)
-        form = app.helpers.point_form.PointForm(request.form, obj=point) 
+        form = app.helpers.point_form.PointForm(request.form, obj=point)
         if form.validate_on_submit():
             form.populate_obj(point)
             db.session.add(point)
@@ -59,7 +109,7 @@ class PointsController:
         return 'Error'
 
     def save_image(self,submission_id, encoded_string):
-        directory = "app/uploads/submissions/" + submission_id 
+        directory = "app/uploads/submissions/" + submission_id
         if not os.path.exists(directory):
             os.makedirs(directory)
         fh = open(directory + "/test.png", "wb")
