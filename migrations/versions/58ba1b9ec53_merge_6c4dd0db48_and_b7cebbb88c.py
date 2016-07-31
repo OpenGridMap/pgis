ad85ac063767b14dc25f39b608608377ae7a0b32
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-07-31 19:49:34 +0200

    Resolve and merge albemic branches.
    There were two different heads and +./manage db upgrade+ fails to work
    with the message:
     FAILED: Multiple head revisions are present for given argument 'head';
     please specify a specific
       target revision, '<branchname>@head' to narrow to a specific head, or
       'heads' for all heads
    
    This was because there were two heads and this has to be resolved by
    merging the heads.
    Refer: http://alembic.zzzcomputing.com/en/latest/branches.html

---
+++ migrations/versions/58ba1b9ec53_merge_6c4dd0db48_and_b7cebbb88c.py
@@ -0,0 +1,22 @@
+"""merge 6c4dd0db48 and b7cebbb88c
+
+Revision ID: 58ba1b9ec53
+Revises: ('6c4dd0db48', 'b7cebbb88c')
+Create Date: 2016-07-31 19:33:28.615540
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '58ba1b9ec53'
+down_revision = ('6c4dd0db48', 'b7cebbb88c')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
