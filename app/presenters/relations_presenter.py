1b327c65e77b9b9133828f0ddd0d9201156d301b
Sri Vishnnu Totakura <srivishnu@totakura.in>
2016-09-07 11:07:04 +0200

    Relations exporters exports to OSM XML format
    http://wiki.openstreetmap.org/wiki/OSM_XML

--- app/presenters/relations_presenter.py
+++ app/presenters/relations_presenter.py
@@ -1,5 +1,7 @@
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 import json
+from app import db
+from sqlalchemy.sql import text
 
 # Presenter logic for Relations (plural).
 class RelationsPresenter:
@@ -7,53 +9,113 @@ class RelationsPresenter:
         self.relations = relations
 
     def as_xml_element(self):
-        relations_elem = Element('relations')
+        osm_elem = Element('osm')
+
+        # ref_node_osmids: osm Ids of nodes that are part of a way
+        # relation_member_node_osmids: osm Ids of nodes that are members of a relation
+        #   We remove relation_member_node_osmids from ref_node_osmids later in
+        #   the logic so that we don't repeat the same nodes in the XML.
+        ref_node_osmids = []
+        relation_member_node_osmids = []
 
         for relation_id, relation in self.relations.items():
-            relation_elem = SubElement(relations_elem, 'relation', {
-                'id': str(relation['id']),
-                'osmid': str(relation['properties']['osmid']),
+            relation_elem = SubElement(osm_elem, 'relation', {
+                'pgisId': str(relation['id']),
+                'id': str(relation['properties']['osmid']),
             })
             self.__buildXmlSubElementForTags(
                 relation['properties']['tags'],
                 relation_elem
             )
 
-            points_elem = SubElement(relation_elem, 'points')
             for point in relation['points']:
-                point_elem = SubElement(points_elem, 'point', {
+                point_elem = SubElement(osm_elem, 'node', {
                     'lat': str(point['latlng'][0]),
                     'lng': str(point['latlng'][1]),
-                    'osmid': str(point['properties']['osmid']),
-                    'id': str(point['id'])
+                    'id': str(point['properties']['osmid']),
+                    'pgisId': str(point['id'])
                 })
                 self.__buildXmlSubElementForTags(
                     point['properties']['tags'],
                     point_elem
                 )
+                relation_member_node_osmids.append(str(point['properties']['osmid']))
 
-            powerlines_elem = SubElement(relation_elem, 'powerlines')
-            for powerline in relation['powerlines']:
-                powerline_elem = SubElement(powerlines_elem, 'powerline', {
-                    'id': str(powerline['id'])
+                member_elem = SubElement(relation_elem, 'member', {
+                    'type': "node",
+                    'ref': str(point['properties']['osmid']),
+                    'role': ""
                 })
 
-                for latlng in powerline['latlngs']:
-                    latlng_elem = SubElement(powerline_elem, 'point', {
-                        'lat': str(latlng[0]),
-                        'lng': str(latlng[1])
+            for powerline in relation['powerlines']:
+
+                # our initial import scripts didn't import OSM IDs for powerlines
+                if 'osmid' in powerline['properties']:
+                    powerline_omsid = powerline['properties']['osmid']
+                else:
+                    powerline_omsid = "-1"
+
+                powerline_elem = SubElement(osm_elem, 'way', {
+                    'id': str(powerline_omsid),
+                    'pgisId': str(powerline['id'])
+                })
+
+                for point_id in powerline['properties']['refs']:
+                    ref_elem = SubElement(powerline_elem, 'nd', {
+                        'ref': str(point_id)
                     })
+                    ref_node_osmids.append(str(point_id))
 
                 self.__buildXmlSubElementForTags(
                     powerline['properties']['tags'],
                     powerline_elem
                 )
 
-        return tostring(relations_elem)
+                member_elem = SubElement(relation_elem, 'member', {
+                    'type': "way",
+                    'ref': str(powerline_omsid),
+                    'role': ""
+                })
+
+        to_be_added_nodes = list(set(ref_node_osmids) - set(relation_member_node_osmids))
+        ref_points = self.__points_with_osmids(to_be_added_nodes)
+
+        for ref_point in ref_points:
+            point_elem = SubElement(osm_elem, 'node', {
+                'lat': str(ref_point['latlng'][0]),
+                'lng': str(ref_point['latlng'][1]),
+                'id': str(ref_point['properties']['osmid']),
+                'pgisId': str(ref_point['id'])
+            })
+            self.__buildXmlSubElementForTags(
+                ref_point['properties']['tags'],
+                point_elem
+            )
+
+        return tostring(osm_elem)
 
     def __buildXmlSubElementForTags(self, tags, parent_xml_element):
         for tag, value in tags.items(): # tag.items() because tags is a dict
             tag_elem = SubElement(parent_xml_element, 'tag', {
-                'key': tag,
-                'value': value
+                'k': tag,
+                'v': value
             })
+
+    def __points_with_osmids(self, osmids):
+        query = text("""
+            SELECT p.id AS p_id, ST_X(p.geom) AS p_x, ST_Y(p.geom) AS p_y,
+                    p.properties AS p_prop
+            FROM point p
+            WHERE properties->>'osmid' IN :osm_ids
+        """)
+        point_rows = db.engine.execute(query, osm_ids = tuple(osmids))
+        ref_points = []
+        for row in point_rows:
+            ref_points.append({
+                'id': row['p_id'],
+                'latlng': [row['p_x'], row['p_y']],
+                'properties': row['p_prop']
+            })
+
+        return ref_points
+
