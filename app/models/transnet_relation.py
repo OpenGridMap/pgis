de8f9e98a2557331532ea72f63ed4fd89f8f968d
Pezhman Nasirifard <epezhman@gmail.com>
2016-10-28 14:00:23 +0200

    import

--- app/models/transnet_relation.py
+++ app/models/transnet_relation.py
@@ -254,31 +254,31 @@ class TransnetRelation(db.Model):
                     else:
                         country_stat['length_by_voltages'][voltage[0]].append(0)
 
-                stations_to_eval = TransnetStation.query.filter(TransnetStation.country == country).options(
-                    load_only("lon", "lat")).all()
-
-                scigrid_all_station_count_query = '''SELECT count(*) as count
-                                                                FROM scigrid_station s
-                                                                WHERE s.type ~ 'station|substation|merge|sub_station|plant' and %s''' % where_clause
-                s_count = [x[0] for x in db.engine.execute(scigrid_all_station_count_query)][0]
-
-                transnet_hit_join_count_query = '''SELECT count(*)
-                                        FROM (
-                                          SELECT t.lon, t.lat
-                                          FROM transnet_station t
-                                          WHERE t.country = '%s'
-                                        ) ts
-                                        JOIN (
-                                            SELECT s.geom_str
-                                            FROM scigrid_station s
-                                            WHERE s.type ~ 'station|substation|merge|sub_station|plant' and %s
-                                        ) ss ON st_dwithin(ST_GeographyFromText('SRID=4326;POINT(' || ts.lon || ' ' || ts.lat || ')'),
-                                                          ST_GeographyFromText(ss.geom_str), 100);''' % (
-                    country, where_clause)
-
-                transnet_hit_join_count_count = [x[0] for x in db.engine.execute(transnet_hit_join_count_query)][0]
-
-                country_stat['plant_station_hit_rate'] = (transnet_hit_join_count_count / s_count) * 100
+                # stations_to_eval = TransnetStation.query.filter(TransnetStation.country == country).options(
+                #     load_only("lon", "lat")).all()
+                #
+                # scigrid_all_station_count_query = '''SELECT count(*) as count
+                #                                                 FROM scigrid_station s
+                #                                                 WHERE s.type ~ 'station|substation|merge|sub_station|plant' and %s''' % where_clause
+                # s_count = [x[0] for x in db.engine.execute(scigrid_all_station_count_query)][0]
+                #
+                # transnet_hit_join_count_query = '''SELECT count(*)
+                #                         FROM (
+                #                           SELECT t.lon, t.lat
+                #                           FROM transnet_station t
+                #                           WHERE t.country = '%s'
+                #                         ) ts
+                #                         JOIN (
+                #                             SELECT s.geom_str
+                #                             FROM scigrid_station s
+                #                             WHERE s.type ~ 'station|substation|merge|sub_station|plant' and %s
+                #                         ) ss ON st_dwithin(ST_GeographyFromText('SRID=4326;POINT(' || ts.lon || ' ' || ts.lat || ')'),
+                #                                           ST_GeographyFromText(ss.geom_str), 100);''' % (
+                #     country, where_clause)
+                #
+                # transnet_hit_join_count_count = [x[0] for x in db.engine.execute(transnet_hit_join_count_query)][0]
+                #
+                # country_stat['plant_station_hit_rate'] = (transnet_hit_join_count_count / s_count) * 100
 
             except Exception as ex:
                 print(ex)
